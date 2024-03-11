99724ed4d71f8a51ec7a79aed1ce177929f829ff
Sofie Cromheeke <sofie.cromheeke@gmail.com>
2024-03-11 22:50:17 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='geonx',
+      version='0.2',
+      description='Analysis of geospatial network data',      
+      author='Sofie Cromheeke',
+      author_email='sofie.cromheeke@stad.gent',
+      packages=find_packages()
+)
